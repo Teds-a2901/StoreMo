1834719bd777c2b17bfa33c6d01d6fc48e619ed3
Teds <marktedra29@gmail.com>
2025-07-17 17:43:40 +0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="text-3xl font-bold text-brand">
+        SroteMo - The Only storage solution you need.
+      </h1>
+    </div>
+  );
+}
